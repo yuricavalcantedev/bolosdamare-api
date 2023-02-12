4a5fbfb62a77083f93752ca5bf63ec88b616a471
Yuri Cavalcante <yuricavalcante17@gmail.com>
2023-02-12 00:49:19 +0000

    Basic project structure - here we go!

---
+++ src/main/java/com/yuri/development/bolos/mare/BolosDaMareApplication.java
@@ -0,0 +1,13 @@
+package com.yuri.development.bolos.mare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BolosDaMareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BolosDaMareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yuri/development/bolos/mare/BolosDaMareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuri.development.bolos.mare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BolosDaMareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
